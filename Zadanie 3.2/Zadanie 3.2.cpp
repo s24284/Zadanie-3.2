2e2368e7c29baf927d79932e6450336131360432
Anna Prymas <s24284@pjwstk.edu.pl>
2021-11-07 17:07:08 +0100

    Dodaj pliki projektów.

---
+++ Zadanie 3.2/Zadanie 3.2.cpp
@@ -0,0 +1,33 @@
+﻿// Zadanie 3.2.cpp 
+//
+
+#include <iostream>
+
+int main()
+{
+	char znak;
+
+	do
+	{
+		
+		std::cout << "Podaj swoj znak. Jezeli podasz ten, ktory chce, dam tobie spokoj: \n";
+	    std::cin >> znak; //użytkownik podaje znak
+	
+
+		if (znak != 't') //reaguje, gdy poda się znak inny od t
+		{
+			std::cout << "To nie to, sprobuj jeszcze raz!\n";
+
+		}
+
+	}
+
+	while (znak != 't');
+	
+		
+		std::cout << "Brawo, to moj znak. Milego dnia! \n";
+	
+
+	return 0;
+}
+
